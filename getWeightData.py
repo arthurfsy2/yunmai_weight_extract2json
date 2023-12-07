484bd90998a54aac8e2fb9b2fa5ca8f08906e834
arthurfsy2 <fsyflh@gmail.com>
2023-12-07 21:02:16 +0800

    优化代码

--- getWeightData.py
+++ getWeightData.py
@@ -89,12 +89,7 @@ def get_accesstoken_request(url, headers,payload):
     json_data = response.json()
     return json_data
 
-#构建请求参数
-def send_post_request(url, headers,payload):
-    
-    response = requests.post(url, headers=headers, data=payload)
-    json_data = response.json()
-    return json_data
+
 
 
 #构建chart-list.json获取重量数据请求参数
@@ -120,24 +115,36 @@ def get_refresh_token(account_b64, account_URI, password_RSA, password_URI,nickn
     #首次登陆，使用虚拟deviceUUID、userId
     deviceUUID = "abcd"
     userId = "199999999"
-
-    loginsign = construct_loginsign(account_b64, password_RSA, deviceUUID, userId)
-    #print(f"loginsign: {loginsign}\n")
     
-    payload = construct_login_payload(password_URI, account_URI, deviceUUID, userId, loginsign)
-    login_result = send_post_request(url, headers, payload)
-    #取返回结果数值
+    # 1. 构建loginSign
+    timestamp = str(int(time.time()))
+    code = timestamp[:8] + "00"
+    userName = account_b64
+    password = password_RSA
+    loginsign = "code=" + code + "&deviceUUID=" + deviceUUID + "&loginType=1&password=" + password + "\n&signVersion=3&userId=" + userId + "&userName=" + userName + "\n&versionCode=7&secret=AUMtyBDV3vklBr6wtA2putAMwtmVcD5b"
+
+    # 2. 构建login请求头data:payload
+    timestamp = str(int(time.time()))
+    code = timestamp[:8] + "00"
+    payload = (
+        "password=" + password_URI + "%0A" +
+        "&code=" + code +
+        "&loginType=1&userName=" + account_URI + "%0A" +
+        "&deviceUUID=" + deviceUUID +
+        "&versionCode=7&userId=" + userId +
+        "&signVersion=3&sign=" + md5(loginsign)
+    )
+
+    # 3. 解析登陆结果，获取refreshToken、userId_real
+    response = requests.post(url, headers=headers, data=payload)
+    login_result = response.json()
     login_stat= login_result['result']['msg']
     print("————————————————————")  
     if login_result['result']['code'] == 0:   
         print(f"{nickname}登陆状态: {login_stat}")
         userId_real= login_result['data']['userinfo']['userId']
-        #print(f"userId_real: {userId_real}\n")
         realName= login_result['data']['userinfo']['realName']
-        #print(f"realName: {realName}\n")
         refreshToken = login_result['data']['userinfo']['refreshToken']
-        #print(f"refreshToken: {refreshToken}\n")
-        
     else:
         print(f"登陆状态: {login_stat}")
         print(f"获取数据失败，已退出")
@@ -173,17 +180,10 @@ def get_access_token(payload):
     return accessToken
 
 def getUserData(accessToken,payload,userId_real,nickname):
-    timestamp = str(int(time.time()))
-    timestamp_past = str(int(time.time()) - 9999 * 24 * 60 * 60) #取当前时间前9999天为需要截取的时间段
-    code = timestamp
-    startTime = timestamp_past
-    #构建chart-list.json请求链接
+    code = str(int(time.time()))
+    startTime = str(int(time.time()) - 9999 * 24 * 60 * 60) #取当前时间前9999天为需要截取的时间段
     data_url = f"https://data.iyunmai.com/api/ios/scale/chart-list.json?code={code}&signVersion=3&startTime={startTime}&userId={userId_real}&versionCode=2"
-
-    #print(f"data_url: {data_url}\n\n")
-    # tokensign = construct_token_sign(account_b64, password_RSA, refreshToken)
-    # payload = construct_token_payload(tokensign, refreshToken)
-    #构建chart-list.json请求头
+    # 1. 构建chart-list.json请求头
     data_headers = {
         'Host': 'account.iyunmai.com',
         'Content-Type': 'application/x-www-form-urlencoded',
@@ -195,14 +195,13 @@ def getUserData(accessToken,payload,userId_real,nickname):
         'IssignV1': 'open',
         'Accept-Language': 'zh-Hans-CN;q=1, en-CN;q=0.9',
     }
-    # 获取体重数据并保存结果
-    
-    getWeight_stat = get_Weight_request(data_url, data_headers,payload)
+    # 2. 获取体重数据
+    response = requests.get(data_url, headers=data_headers, data=payload)
+    getWeight_stat = response.json()
+    # 3. 解析体重数据并保存
     weight_data = getWeight_stat['data']['rows']
     json_data = json.dumps(weight_data,indent=2)
-
     #print(f"weight_data: {weight_data}\n\n")
-
     with open(f'weight_{nickname}.json', 'w',encoding="utf-8") as f:
         #json_data = black.format_str(json_data, mode=black.FileMode())
         f.write(json_data)
@@ -210,11 +209,14 @@ def getUserData(accessToken,payload,userId_real,nickname):
         print("————————————————————")
 
 def getUserInfo(account, password,nickname):
+    # 1. 加密账号密码
     account_b64, account_URI, password_RSA, password_URI = encrypt_account_password(account, password)
-    # 获取refreshToken
+    # 2. 获取refreshToken
     refreshToken,userId_real = get_refresh_token(account_b64, account_URI, password_RSA, password_URI,nickname)
     payload = construct_token_payload(refreshToken)
+    # 3. accessToken
     accessToken = get_access_token(payload)
+    # 4. 获取体重数据
     getUserData(accessToken,payload,userId_real,nickname)
 
 
